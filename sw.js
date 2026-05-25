4dd3f7bd1b22f3903bf20492d5eea35c70ef02eb
Daniel Vitas <daniel@Daniels-MacBook-Air.local>
2026-05-25 18:34:43 +0200

    update webpage

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@
 // entire cache atomically on the next visit. Old caches are deleted
 // during the activate phase.
 
-const CACHE_VERSION = 'mat-tikz-30-43-66';
+const CACHE_VERSION = 'mat-tikz-30-43-v2';
 const STATIC_CACHE   = `mat-static-${CACHE_VERSION}`;
 const HTML_CACHE     = `mat-html-${CACHE_VERSION}`;
 const EXTERNAL_CACHE = `mat-ext-${CACHE_VERSION}`;
